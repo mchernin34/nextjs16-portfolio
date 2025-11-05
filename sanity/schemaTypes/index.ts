5032caf521c48c5e7cf62a88e735e4fd9cfff726
Michael Chernin <mchernin21@gmail.com>
2025-11-05 16:38:58 +0200

    Initial commit

---
+++ sanity/schemaTypes/index.ts
@@ -0,0 +1,19 @@
+import { type SchemaTypeDefinition } from 'sanity'
+
+export const schema: { types: SchemaTypeDefinition[] } = {
+  types: [
+    profile,
+    project,
+    skill,
+    experience,
+    education,
+    testimonial,
+    certification,
+    achievement,
+    blog,
+    service,
+    contact,
+    siteSettings,
+    navigation
+  ],
+}
